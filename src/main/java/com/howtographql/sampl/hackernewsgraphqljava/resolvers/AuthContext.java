4057a941b28e1b3526ac8cb2457fc577b7e529ea
Alexander Pyreev <a.s.pyreev@gmail.com>
2018-08-01 12:50:38 +0300

    ass auth context

---
+++ src/main/java/com/howtographql/sampl/hackernewsgraphqljava/resolvers/AuthContext.java
@@ -0,0 +1,19 @@
+package com.howtographql.sampl.hackernewsgraphqljava.resolvers;
+
+import com.howtographql.sampl.hackernewsgraphqljava.model.User;
+import graphql.servlet.GraphQLContext;
+import lombok.Getter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Optional;
+
+@Getter
+public class AuthContext extends GraphQLContext {
+    private final User user;
+
+    public AuthContext(User user, Optional<HttpServletRequest> request, Optional<HttpServletResponse> response) {
+        super(request, response);
+        this.user = user;
+    }
+}

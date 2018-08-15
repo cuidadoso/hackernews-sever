61ea8be4de00777cdc981366e7dc863e074a0aad
Alexander Pyreev <a.s.pyreev@gmail.com>
2018-08-15 16:11:43 +0300

    enable websocket

---
+++ src/main/java/com/howtographql/sampl/hackernewsgraphqljava/configurations/AppBeans.java
@@ -0,0 +1,32 @@
+package com.howtographql.sampl.hackernewsgraphqljava.configurations;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
+import org.springframework.web.socket.config.annotation.AbstractWebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.springframework.context.annotation.ScopedProxyMode.TARGET_CLASS;
+
+@Configuration
+public class AppBeans {
+    @Bean
+    @Scope(value = "singleton", proxyMode = TARGET_CLASS)
+    public Map<String, Object> store() {
+        return new HashMap<>();
+    }
+
+    @Bean
+    @Scope(value = "singleton", proxyMode = TARGET_CLASS)
+    public AbstractWebSocketMessageBrokerConfigurer webSocket() {
+        return new AbstractWebSocketMessageBrokerConfigurer() {
+            @Override
+            public void registerStompEndpoints(StompEndpointRegistry registry) {
+                registry.addEndpoint("/subscriptions").withSockJS();
+            }
+        };
+    }
+}
